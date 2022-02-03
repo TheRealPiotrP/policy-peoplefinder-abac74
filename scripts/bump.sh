eb676999df01426c96a953275c3ca2a8fca9a1bd
Piotr Puszkiewicz <piotr@aserto.com>
2022-02-03 05:28:21 +0000

    0.0.3-dirty

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,5 +1,6 @@
 echo "# `sver -n patch`" >> ./src/policies/get.rego
 git add .
 git commit -m "`sver -n patch`"
+git tag `sver -n patch`
 git push
 git push --tags
--- src/policies/get.rego
+++ src/policies/get.rego
@@ -13,3 +13,4 @@ default enabled = true
 # 0.0.2
 # 0.0.2
 # 0.0.2
+# 0.0.3-dirty
